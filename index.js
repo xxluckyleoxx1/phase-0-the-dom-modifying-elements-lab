61788844c0120f0c1d3195203daa11c40896fb69
github username <github email address>
2021-03-30 18:08:44 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Leo is the champion";
+newHeader.appendChild(newHeader);
